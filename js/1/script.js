3441ff5b696365bb57603e8dfc4ef0bc56dce119
giubertsanchez <56102914+giubertsanchez@users.noreply.github.com>
2023-10-22 18:55:50 -0400

    while loop example

--- js/1/script.js
+++ js/1/script.js
@@ -4,3 +4,10 @@ for (let i = 0; i < 5; i++) {
     console.log("hello")
 }
 
+// code for example 2
+console.log("example 2")
+let i = 0;
+while (i < 5) {
+    console.log("while loop hello " + i)
+    i++
+}
