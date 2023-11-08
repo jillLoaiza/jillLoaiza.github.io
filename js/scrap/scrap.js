cff03b90fd5d1a0f6c7a5170cdb9f85b2b5fcd44
giubertsanchez <56102914+giubertsanchez@users.noreply.github.com>
2023-11-08 11:36:52 -0500

    code blocks

---
+++ js/scrap/scrap.js
@@ -0,0 +1,37 @@
+function greet1() {
+    console.log('Hi')
+}
+
+const greetA1 = () => {
+    console.log("Hi")
+}
+
+function mult(num1, num2) {
+    console.log(num1 * num2)
+}
+const multA = (num1, num2) => {
+    console.log(num1 * num2)
+}
+
+function myArr(arr) {
+    console.log(arr.length)
+}
+const myArrA = (arr) => {
+    console.log(arr.length)
+}
+
+function greet2(name) {
+    console.log('Hello ' + name)
+}
+const greetA2 = (name) => {
+    console.log("Hello " + name)
+}
+
+function bark() {
+    console.log('Bark')
+}
+
+const barkA = () => {
+    console.log("Bark")
+}
+
